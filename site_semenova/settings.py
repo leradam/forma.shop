348c863852b0110bd2d433f013c0c1d87b7ed08f
YumS <jumkaa57@gmail.com>
2026-05-07 05:30:40 +0800

    Встроен CSS в base.html, обновлены настройки и конфиги

--- site_semenova/settings.py
+++ site_semenova/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-sck%4xefd8i16nhriy45hvm-d1$+$k4gjywh86+b1v!yv0$wdo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.115.130', 'localhost', '127.0.0.1']
 
 
 # Application definition
